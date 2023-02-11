fefb5cec20aaab8f4d17bea03ab089d44ce40554
Ervin Castro Perez <ervindevc@gmail.com>
2023-02-10 21:51:27 -0500

    cracionde la ruta movies y metodo Get

--- README.md
+++ README.md
@@ -104,6 +104,36 @@ Los principales metodos usados por una **API** es:
 * **_GET:_** consulta informaciòn de un recurso.
 * **_DELETE:_** Elimina un recurso
 
+## creación de la ruta movies y metodo GET
 
+```sh
+from fastapi import FastAPI
+from fastapi.responses import HTMLResponse #importaciòn de la clase para utilizar html
+
+app = FastAPI() 
+app.title = "My aplicación con FastAPI" 
+app.version = "0.0.1" 
+
+movies = [
+    {
+        'id': 1,
+        'title': 'Avatar',
+        'overview': "En un exuberante planeta llamado Pandora viven los Na'vi, seres que ...",
+        'year': '2009',
+        'rating': 7.8,
+        'category': 'Acción'    
+    } 
+]
+#creacion del endpoin
+@app.get('/', tags=['home']) 
+def message():
+    return HTMLResponse('<h1>Hello world</h1>') #utilizando html
+
+#creaciòn de la ruta peliculas, y la etiqueta peliculas
+@app.get('/movies', tags=['movies'])
+def get_movies(): #devuelve el listado de las peliculas
+    return movies
+
+```
 
 
--- main.py
+++ main.py
@@ -1,13 +1,30 @@
 from fastapi import FastAPI
+from fastapi.responses import HTMLResponse #importaciòn de la clase para utilizar html
 
-app = FastAPI()  #creación de una instancia
-app.title = "My aplicación con FastAPI"  #Colocar nombre a la app
-app.version = "0.0.1" #Colocar version en especifico
+app = FastAPI() 
+app.title = "My aplicación con FastAPI" 
+app.version = "0.0.1" 
 
+movies = [
+    {
+        'id': 1,
+        'title': 'Avatar',
+        'overview': "En un exuberante planeta llamado Pandora viven los Na'vi, seres que ...",
+        'year': '2009',
+        'rating': 7.8,
+        'category': 'Acción'    
+    } 
+]
 #creacion del endpoin
-@app.get('/', tags=['home']) #Se agrega el home para agrupar determinadas rutas
+@app.get('/', tags=['home']) 
 def message():
-    return "Hello world! desde mi celular"
+    return HTMLResponse('<h1>Hello world</h1>') #utilizando html
+
+#creaciòn de la ruta peliculas, y la etiqueta peliculas
+@app.get('/movies', tags=['movies'])
+def get_movies(): #devuelve el listado de las peliculas
+    return movies
+
 
 
 
